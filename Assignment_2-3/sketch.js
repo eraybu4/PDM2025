3c3dfe79656d634f7382863f8f661a6c53a3c9a7
eraybu4 <145386577+eraybu4@users.noreply.github.com>
2025-03-11 11:53:47 -0500

    Add files via upload

---
+++ Assignment_2-3/sketch.js
@@ -0,0 +1,63 @@
+let noise1, noiseEnv, filt1;
+let fireworks = [];
+let img;
+
+function preload() {
+  img = (loadImage('media/firework.png'));
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  filt1 = new Tone.Filter({
+    frequency: 3000,  
+    type: "lowpass",
+    rolloff: -24,
+  }).toDestination();
+
+  noise1 = new Tone.Noise("white");
+
+  noiseEnv = new Tone.AmplitudeEnvelope({
+    attack: 0.01,  
+    decay: 1.5,    
+    sustain: 0,    
+    release: 0.7   
+  });
+  
+  
+  noise1.connect(noiseEnv);
+  noiseEnv.connect(filt1);
+  
+  
+  noise1.start();
+}
+
+function draw() {
+  background(0);
+
+     // Display active fireworks.
+  for (let i = fireworks.length - 1; i >= 0; i--) {
+    let fw = fireworks[i];
+    imageMode(CENTER);
+    
+    image(img, fw.x, fw.y, 100, 102);
+    fw.timer--; 
+    
+    
+    if (fw.timer <= 0) {
+      fireworks.splice(i, 1);
+    }
+  }
+}
+
+function mouseClicked() {
+  
+    
+    noiseEnv.triggerAttackRelease(0.5);
+
+
+    fireworks.push({ x: mouseX, y: mouseY, timer: 60 });
+    
+    console.log('Explosion sound triggered');
+  
+}
